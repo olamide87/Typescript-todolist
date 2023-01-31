7e2900b0cad110ddcb0cd92a04cfb4f18750a5f1
Olamide Oladinni <olajide1987@gmail.com>
2023-01-30 23:51:17 -0600

    added input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
-    Cruel Committee
+    <span className="heading">Taskify</span>
+    <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
