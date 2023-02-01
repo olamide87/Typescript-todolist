2e54ac74ee9c201382c01559ebb427cfe7c5d945
Olamide Oladinni <olajide1987@gmail.com>
2023-02-01 00:00:12 -0600

    wip

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import './styles.css'
 
 interface Props {
     todo: string;
     setTodo:  React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void; 
 }
 
-const InputField = ({ todo, setTodo }:Props) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }:Props) => {
+    
   return (
-    <form className='input'>
+    <form className='input'
+    onSubmit=
+        {handleAdd}>
         <input 
         type='input' 
         value={todo} 
